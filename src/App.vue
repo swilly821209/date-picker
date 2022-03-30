fb8a669d556c802670a24f8dcdaa5fcb02296e6b
willy <swilly821209@icloud.com>
2022-03-30 22:27:51 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import dayjs from "dayjs";
+import DatePicker from "@/components/DatePicker.vue";
+let today = dayjs();
+let aa = "aa";
+let aad = "a";
+</script>
+
+<template>
+  <date-picker v-model="today"></date-picker>
+</template>
